c6d82cd46e174d0d24c28808e6b1b3ece4afddea
fox <fox@mail.com>
2026-07-26 01:16:53 +0300

    Added Task2

---
+++ src/chapter1/Task2.java
@@ -0,0 +1,17 @@
+package chapter1;
+/** Первая программа-пример книги
+ * Выводит струку и текущее число
+ * @author Брюс Эккель
+ * @author www.MindViev.net
+ * @version 4.0
+ */
+public class Task2 {
+    /** Точка входа в класс и приложение
+     * @param args Массив строковых аргументов
+     * @throws exceptions Исключения не выдаются
+     */
+    public static void main(String[] args) {
+        System.out.println("Привет мир");
+    }
+
+}
