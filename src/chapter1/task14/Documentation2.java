3981beb432bfb9bfc05775a1a7eccd44c9feb84c
fox <fox@mail.com>
2026-07-31 20:00:29 +0300

    Update Task14

---
+++ src/chapter1/task14/Documentation2.java
@@ -0,0 +1,17 @@
+package chapter1.task14;
+
+//: object/Documentation2.java
+/**
+ * <pre>
+ * System.out.println(new Date());
+ * </pre>
+ * <p>Компоненты кода:
+ * <ol>
+ * <li> {@code System} - класс для работы с системой
+ * <li> {@code out} - стандартный поток вывода
+ * <li> {@code println()} - метод вывода текста
+ * <li> {@code Date} - класс для работы с датой и временем
+ * </ol>
+ */
+public class Documentation2 {}
+///:~
