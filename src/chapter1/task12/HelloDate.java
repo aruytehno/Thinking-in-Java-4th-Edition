a5d986860c9422c22b4fc9f5b918bfdeedf819c1
fox <fox@mail.com>
2026-07-31 16:09:29 +0300

    Added Task12

---
+++ src/chapter1/task12/HelloDate.java
@@ -0,0 +1,20 @@
+package chapter1.task12;
+
+import java.util.Date;
+
+/** Первая программа-пример книги
+ * Выводит струку и текущее число
+ * @author Брюс Эккель
+ * @author www.MindViev.net
+ * @version 4.0
+ */
+public class HelloDate {
+    /** Точка входа в класс и приложение
+     * @param args Массив строковых аргументов
+     * @throws exceptions Исключения не выдаются
+     */
+    public static void main(String[] args) {
+        System.out.println("Привет, сегодня: ");
+        System.out.println(new Date());
+    }
+}
