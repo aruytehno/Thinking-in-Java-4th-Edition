dee9db109e4264406ef4f28d2f57e5b148c10229
fox <fox@mail.com>
2026-07-31 19:39:25 +0300

    Added Task13

---
+++ src/chapter1/task13/Documentation1.java
@@ -0,0 +1,13 @@
+package chapter1.task13;
+
+//: object/Documentation1.java
+/** A class comment */
+public class Documentation1 {
+    /** A field comment */
+    public int i;
+    /** A method comment */
+    public void f() {}
+} ///:~
+
+// Указать папку для документации: -d docs
+// javadoc -d docs *.java
