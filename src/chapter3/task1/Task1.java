c3d9dc9e5b40059c16a633e4eb5c5b87a5627de9
fox <fox@mail.com>
2026-08-03 17:05:36 +0300

    Added task 1, from chapter 2

---
+++ src/chapter3/task1/Task1.java
@@ -0,0 +1,11 @@
+package chapter3.task1;
+
+public class Task1 {
+    static void print(Object obj) {
+        System.out.println(obj);
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        print("Hi");
+    }
+}
